87ea294d050c3c67c5ac934a37ffd1847bf737a6
Carlos18v <25030207@itesa.edu.mx>
2026-09-17 07:27:17 -0600

    version_02:Metodollenar arreglo

---
+++ ArreglosG3_T3/Arreglos.Logica/MiArreglo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arreglos.Logica
+{
+    public class MiArreglo
+    {
+        //campos o atributos
+        private int _tope;
+        private int[] _arreglo;
+        //constructor
+        public MiArreglo(int n)
+        {
+            N = n;
+            _arreglo = new int[n];
+            _tope = 0;
+        }
+        //propiedades
+        public int N { get; }
+
+        public bool EstaLeno => _tope == N;
+        public bool EstaVacio => _tope == 0;
+        //metodos
+        public void Llenar(int minimo, int maximo) { 
+        Random random = new Random();
+            for (int i = 0; i < N; i++) { 
+            _arreglo[i] = random.Next(minimo, maximo);
+
+            }
+            _tope = N;
+        }
+        public override string ToString()
+        {
+            if (EstaVacio) { 
+            Console.WriteLine("El arreglo esta vacio");
+            }
+            string salida = string.Empty;
+            for (int i = 0; i < N; i++)
+            {
+                salida += $"{_arreglo[i]}\t ";
+            }
+            return salida;
+        }
+    }
+}
--- ArreglosG3_T3/Arreglos.UI.Consola/Program.cs
+++ ArreglosG3_T3/Arreglos.UI.Consola/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+using Arreglos.Logica;
+
+Console.WriteLine("Arreglos");
+MiArreglo oMyarreglo = new(10000);
+oMyarreglo.Llenar(5,50);
+oMyarreglo.ToString();
+Console.WriteLine(oMyarreglo.ToString());
+Console.ReadKey();
